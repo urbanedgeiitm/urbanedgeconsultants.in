68474b53de066122d2f22b41171794152fd384c7
urbanedgeiitm <urbanedgerdconsultants@gmail.com>
2024-09-29 00:25:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    color: #333;
+    padding: 20px;
+}
+window.onload = function() {
+    alert('Website is loaded successfully!');
+}
